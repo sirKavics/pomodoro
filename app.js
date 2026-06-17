c283b593f0793d85629197b374bcfb5b1b52ce6b
sirkavics <falf555@gmail.com>
2026-06-16 23:31:16 -0400

    clock countdown functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let cancelId;
 let startTime;
-const countdown = 25 * 60 * 1000;
+let savedTime = 0;
+const countdown = 5 * 1000;
 
 const timerMilliseconds = document.querySelector('.timer__milliseconds');
 const timerSeconds = document.querySelector('.timer__seconds');
@@ -8,22 +9,56 @@ const timerMinutes = document.querySelector('.timer__minutes');
 
 function startTimer() {
     startTime = Date.now();
-    console.log(startTime)
     cancelId =requestAnimationFrame(updateTimer);
 }
 
 function stopTimer() {
     cancelAnimationFrame(cancelId);
+    savedTime += Date.now() - startTime;
+    console.log(savedTime);
 }
 
 function resetTimer() {
+    startTime = Date.now();
+    savedTime = 0;
 
+    timerMilliseconds.innerHTML = "000";
+    timerSeconds.innerHTML = "00";
+    timerMinutes.innerHTML = "25";
 }
 
 function updateTimer() {
-    let millisElapsed = Date.now() - startTime;
+    let millisElapsed = Date.now() - startTime + savedTime;
 
-    let timeLeft = countdown - millisElapsed;
-    console.log(timeLeft);
-    cancelId = requestAnimationFrame(updateTimer);
+    let millisLeft = countdown - millisElapsed;
+    if (millisLeft < 0) {
+        millisLeft = 0;
+        cancelAnimationFrame(cancelId);
+        cancelId = null;
+    }
+
+    let secondsLeft = millisLeft / 1000;
+    let minutesLeft = secondsLeft / 60;
+
+    let millisecondsFormat = millisLeft % 1000;
+    let secondsFormat = Math.floor(secondsLeft) % 60;
+    let minutesFormat = Math.floor(minutesLeft);
+
+    if (millisecondsFormat.toString().length < 3) {
+        millisecondsFormat = millisecondsFormat.toString().padStart(3, "0");
+    }
+    if (secondsFormat.toString().length < 2) {
+        secondsFormat = secondsFormat.toString().padStart(2, "0");
+    }
+    if(minutesFormat.toString().length < 2) {
+        minutesFormat = minutesFormat.toString().padStart(2, "0");
+    }
+    
+    timerMilliseconds.innerHTML = millisecondsFormat;
+    timerSeconds.innerHTML =  secondsFormat;
+    timerMinutes.innerHTML = minutesFormat;
+
+    if (cancelId) {
+        cancelId = requestAnimationFrame(updateTimer);
+    }
 }
