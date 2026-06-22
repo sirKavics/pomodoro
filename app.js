f3d5e78a6687744c72bd75955a51bed422bc434b
sirkavics <falf555@gmail.com>
2026-06-22 19:12:14 -0400

    reset button functionality

--- app.js
+++ app.js
@@ -97,7 +97,12 @@ function stopTimer() {
 }
 
 function resetTimer() {
+    startTime = Date.now();
+    savedTime = 0;
 
+    timerMilliseconds.innerHTML = "000"
+    timerSeconds.innerHTML = "00"
+    timerMinutes.innerHTML = "25"
 }
 
 function updateTimer() {
